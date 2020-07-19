d18dac213b58417f6967f9e7ef5305f6948b5b0a
AP <porwal.1@iitj.ac.in>
2020-07-19 14:48:43 +0530

    final project

---
+++ index.js
@@ -0,0 +1,10 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: './bw1.jpg',
+    image2: './bw2.jpg',
+    // speedIn:3,
+    // speedOut:5,
+    angle: Math.PI /2,
+    displacementImage: './bw3.jpg'
+})
